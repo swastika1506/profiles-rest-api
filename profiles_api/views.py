71a3f826bcf146bb766d32bfd7010cfba7db37cd
swastika <swastikshetty1519@gmail.com>
2023-03-25 00:27:28 +0530

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API view"""
@@ -44,9 +47,11 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
          """Delete an object"""
          return Response({'method': 'DELETE'})
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
     serializer_class=serializers.HelloSerialzer
+
     def list(self,request):
         """Return Hello message"""
         a_viewset=[
@@ -55,6 +60,7 @@ class HelloViewSet(viewsets.ViewSet):
             'Provides more functionality with less code',
             ]
         return Response({'message':'Hello!','a_viewset': a_viewset})
+
     def create(self,request):
         """create a new Hello message"""
         serializer=self.serializer_class(data=request.data)
@@ -84,3 +90,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
